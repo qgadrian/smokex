fa02c2266d6ffe0990eecbbf9870beea62216e76
qgadrian <qgadrian@users.noreply.github.com>
2020-07-02 20:35:44 +0200

    Create plan executions
    For plan definitions

---
+++ apps/smokex/lib/plan_executions.ex
@@ -0,0 +1,24 @@
+defmodule Smokex.PlanExecutions do
+  @moduledoc """
+  Context module to handle plan executions.
+  """
+
+  alias Smokex.PlanExecution
+  alias Smokex.PlanDefinition
+
+  @doc "Creates a new plan execution"
+  @spec create_plan_execution(PlanDefinition.t()) :: {:ok, PlanExecution.t()} | {:error, term}
+  def create_plan_execution(%PlanDefinition{} = plan_definition) do
+    %PlanExecution{}
+    |> PlanExecution.changeset(%{plan_definition: plan_definition})
+    |> Smokex.Repo.insert()
+  end
+
+  @doc "Updates a plan execution"
+  @spec update_plan_execution(PlanExecution.t(), map) :: {:ok, PlanExecution.t()} | {:error, term}
+  def update_plan_execution(%PlanExecution{} = plan_execution, attrs) do
+    plan_execution
+    |> PlanExecution.changeset(attrs)
+    |> Smokex.Repo.update()
+  end
+end
