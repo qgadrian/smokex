194b6d506b51fe216e2f5215a9fe0dbb82f400b9
qgadrian <qgadrian@users.noreply.github.com>
2020-07-07 19:10:07 +0200

    Highlight active filter

--- apps/smokex_web/lib/smokex_web/views/plans_executions/components/filter.ex
+++ apps/smokex_web/lib/smokex_web/views/plans_executions/components/filter.ex
@@ -7,38 +7,54 @@ defmodule SmokexWeb.PlanExecutions.Components.FilterView do
   # active
   @default_class "list-group-item list-group-item-action d-flex justify-content-between align-items-center hover-cursor"
 
-  def plan_execution_filter(:created, plan_executions) do
+  @spec plan_execution_filter(atom, list(PlanExecution.t()), boolean) :: Phoenix.HTML.Tag.t()
+  def plan_execution_filter(:all, plan_executions, active_filter) do
+    plan_executions
+    |> count_by(:all)
+    |> filter_tag("All", active_filter == :all)
+  end
+
+  def plan_execution_filter(:created, plan_executions, active_filter) do
     plan_executions
     |> count_by(:created)
-    |> filter_tag("Created")
+    |> filter_tag("Created", active_filter == :created)
   end
 
-  def plan_execution_filter(:finished, plan_executions) do
+  def plan_execution_filter(:finished, plan_executions, active_filter) do
     plan_executions
     |> count_by(:finished)
-    |> filter_tag("Finished")
+    |> filter_tag("Finished", active_filter == :finished)
   end
 
-  def plan_execution_filter(:halted, plan_executions) do
+  def plan_execution_filter(:halted, plan_executions, active_filter) do
     plan_executions
     |> count_by(:halted)
-    |> filter_tag("Halted")
+    |> filter_tag("Halted", active_filter == :halted)
   end
 
-  def plan_execution_filter(:running, plan_executions) do
+  def plan_execution_filter(:running, plan_executions, active_filter) do
     plan_executions
     |> count_by(:running)
-    |> filter_tag("Running")
+    |> filter_tag("Running", active_filter == :running)
   end
 
-  defp filter_tag(number_of_items, label) do
-    content_tag :a, class: @default_class do
+  defp filter_tag(number_of_items, label, is_active) do
+    class =
+      if is_active do
+        "#{@default_class} active"
+      else
+        @default_class
+      end
+
+    content_tag :a, class: class do
       [
         content_tag(:span, label),
-        content_tag(:span, number_of_items, class: "badge badge-primary badge-pill")
+        content_tag(:span, number_of_items, class: "badge badge-secondary badge-pill")
       ]
     end
   end
+
+  defp count_by(plan_executions, :all), do: length(plan_executions)
 
   defp count_by(plan_executions, status) do
     Enum.count(plan_executions, fn
