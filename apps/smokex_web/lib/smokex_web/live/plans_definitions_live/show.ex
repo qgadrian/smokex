d55407e3903cf1e516b5f7dd4e456e0cb0cc75b4
qgadrian <qgadrian@users.noreply.github.com>
2020-07-07 20:28:40 +0200

    Show details of plan definitions

--- apps/smokex_web/lib/smokex_web/live/plans_definitions_live/show.ex
+++ apps/smokex_web/lib/smokex_web/live/plans_definitions_live/show.ex
@@ -1,13 +1,35 @@
 defmodule SmokexWeb.PlansDefinitionsLive.Show do
   use SmokexWeb, :live_view
 
+  alias Phoenix.LiveView.Socket
   alias Smokex.PlanDefinitions
+  alias Smokex.PlanExecutions
+
+  alias SmokexWeb.PlansExecutionsLive.Components.Table, as: TableComponent
 
   @impl true
   def mount(_params, _session, socket) do
-    all_plan_definitions = PlanDefinitions.all()
+    {:ok, socket}
+  end
 
-    {:ok, assign(socket, plan_definitions: all_plan_definitions)}
+  def handle_params(%{"id" => id}, _url, socket) do
+    # if connected?(socket), do: Demo.Accounts.subscribe(plan_definition_id)
+    socket =
+      socket
+      |> assign(id: id)
+      |> fetch_plan_definition()
+      |> fetch_plan_executions()
+
+    {:noreply, socket}
+  end
+
+  defp fetch_plan_definition(%Socket{assigns: %{id: id}} = socket) do
+    plan_definition = PlanDefinitions.get(id)
+    assign(socket, plan_definition: plan_definition)
+  end
+
+  defp fetch_plan_executions(%Socket{assigns: %{id: id}} = socket) do
+    plan_executions = PlanExecutions.get_by_plan_definition(id, 5)
+    assign(socket, plan_executions: plan_executions)
   end
 end
-
