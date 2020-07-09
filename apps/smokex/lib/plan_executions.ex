b8ad43fb83ac0bd75e4b4a932a9c738ac67cf284
qgadrian <qgadrian@users.noreply.github.com>
2020-07-09 19:04:30 +0200

    Subscribe to plan execution changes

--- apps/smokex/lib/plan_executions.ex
+++ apps/smokex/lib/plan_executions.ex
@@ -13,9 +13,20 @@ defmodule Smokex.PlanExecutions do
   """
   @spec create_plan_execution(PlanDefinition.t()) :: {:ok, PlanExecution.t()} | {:error, term}
   def create_plan_execution(%PlanDefinition{} = plan_definition) do
-    %PlanExecution{}
-    |> PlanExecution.create_changeset(%{plan_definition: plan_definition})
-    |> Smokex.Repo.insert()
+    result =
+      %PlanExecution{}
+      |> PlanExecution.create_changeset(%{plan_definition: plan_definition})
+      |> Smokex.Repo.insert()
+
+    with {:ok, plan_execution} <- result do
+      Phoenix.PubSub.broadcast(
+        Smokex.PubSub,
+        "#{plan_definition.id}",
+        {:created, plan_execution}
+      )
+    end
+
+    result
   end
 
   @doc """
@@ -40,16 +51,20 @@ defmodule Smokex.PlanExecutions do
     Smokex.Repo.all(query)
   end
 
+  # TODO rename this
+  def get_by_plan_definition(plan_definition_id, limit \\ 100)
+
   @doc """
   Returns all the executions of a plan definition id.
   """
   @spec get_by_plan_definition(integer, integer) :: list(PlanExecution.t())
-  def get_by_plan_definition(plan_definition_id, limit) do
+  # TODO add a config to limit
+  def get_by_plan_definition(plan_definition_id, limit) when is_number(limit) do
     query =
       from(plan_execution in PlanExecution,
         where: plan_execution.plan_definition_id == ^plan_definition_id,
         limit: ^limit,
-        order_by: [asc: :finished_at],
+        order_by: [desc: :updated_at],
         select: plan_execution
       )
 
@@ -73,6 +88,42 @@ defmodule Smokex.PlanExecutions do
   end
 
   @doc """
+  Updates a plan execution as started.
+  """
+  @spec start(PlanExecution.t()) :: {:ok, PlanExecution.t()} | {:error, term}
+  def start(%PlanExecution{} = plan_execution) do
+    plan_execution
+    |> update_plan_execution(%{
+      status: :running,
+      started_at: NaiveDateTime.utc_now()
+    })
+    |> notify_change(:started)
+  end
+
+  @doc """
+  Finishes a plan execution and sets it as `halted`.
+  """
+  @spec halt(PlanExecution.t()) :: {:ok, PlanExecution.t()} | {:error, term}
+  def halt(%PlanExecution{} = plan_execution) do
+    plan_execution
+    |> update_plan_execution(%{status: :halted, finished_at: NaiveDateTime.utc_now()})
+    |> notify_change(:halted)
+  end
+
+  @doc """
+  Updates a plan execution as finished.
+  """
+  @spec finish(PlanExecution.t()) :: {:ok, PlanExecution.t()} | {:error, term}
+  def finish(%PlanExecution{} = plan_execution) do
+    plan_execution
+    |> update_plan_execution(%{
+      status: :finished,
+      finished_at: NaiveDateTime.utc_now()
+    })
+    |> notify_change(:halted)
+  end
+
+  @doc """
   Updates a plan execution
   """
   @spec update_plan_execution(PlanExecution.t(), map) :: {:ok, PlanExecution.t()} | {:error, term}
@@ -83,29 +134,27 @@ defmodule Smokex.PlanExecutions do
   end
 
   @doc """
-  Updates a plan execution as started.
+  Subscribes to the plan execution.
   """
-  @spec start(PlanExecution.t()) :: {:ok, PlanExecution.t()} | {:error, term}
-  def start(%PlanExecution{} = plan_execution) do
-    update_plan_execution(plan_execution, %{status: :running, started_at: NaiveDateTime.utc_now()})
+  @spec subscribe(PlanExecution.t()) :: :ok | {:error, term}
+  def subscribe(%PlanExecution{} = plan_execution) do
+    Phoenix.PubSub.subscribe(Smokex.PubSub, "#{plan_execution.id}", link: true)
   end
 
-  @doc """
-  Finishes a plan execution and sets it as `halted`.
-  """
-  @spec halt(PlanExecution.t()) :: {:ok, PlanExecution.t()} | {:error, term}
-  def halt(%PlanExecution{} = plan_execution) do
-    update_plan_execution(plan_execution, %{status: :halted, finished_at: NaiveDateTime.utc_now()})
-  end
+  #
+  # Private functions
+  #
 
-  @doc """
-  Updates a plan execution as finished.
-  """
-  @spec finish(PlanExecution.t()) :: {:ok, PlanExecution.t()} | {:error, term}
-  def finish(%PlanExecution{} = plan_execution) do
-    update_plan_execution(plan_execution, %{
-      status: :finished,
-      finished_at: NaiveDateTime.utc_now()
-    })
+  @spec notify_change(term, PlanExecution.status()) :: term
+  defp notify_change(result, event) do
+    with {:ok, plan_execution} <- result do
+      Phoenix.PubSub.broadcast(
+        Smokex.PubSub,
+        "#{plan_execution.id}",
+        {event, plan_execution}
+      )
+    end
+
+    result
   end
 end
--- apps/smokex/lib/results.ex
+++ apps/smokex/lib/results.ex
@@ -6,9 +6,31 @@ defmodule Smokex.Results do
   alias Smokex.Result
 
   @spec create(map) :: {:ok, Result.t()} | {:error, Ecto.Changeset.t()}
-  def create(attrs) do
-    %Result{}
-    |> Result.changeset(attrs)
-    |> Smokex.Repo.insert()
+  def create(%{plan_execution: plan_execution} = attrs) do
+    result =
+      %Result{}
+      |> Result.changeset(attrs)
+      |> Smokex.Repo.insert()
+      |> notify_result(plan_execution)
+  end
+
+  defp notify_result({:ok, %Result{}} = result, plan_execution) do
+    Phoenix.PubSub.broadcast(
+      Smokex.PubSub,
+      "#{plan_execution.id}",
+      {:result, plan_execution}
+    )
+
+    result
+  end
+
+  defp notify_result({:error, _result_changeset} = result, plan_execution) do
+    Phoenix.PubSub.broadcast(
+      Smokex.PubSub,
+      "#{plan_execution.id}",
+      {:error, plan_execution}
+    )
+
+    result
   end
 end
