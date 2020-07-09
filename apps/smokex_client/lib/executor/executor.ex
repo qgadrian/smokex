2a43e38843ef67c2ddb49afb5abb750eccf8b462
qgadrian <qgadrian@users.noreply.github.com>
2020-07-09 20:40:58 +0200

    Executor module uses PlanExecution

--- apps/smokex_client/lib/executor/executor.ex
+++ apps/smokex_client/lib/executor/executor.ex
@@ -1,22 +1,37 @@
 defmodule SmokexClient.Executor do
+  @moduledoc """
+  This module starts a plan execution.
+
+  The execution will generate results associated to the execution.
+
+  After and before the execution, the [plan execution](`t:#{PlanExecution}`) is
+  updated with the proper status.
+  """
+
+  require Logger
+
   alias SmokexClient.ExecutionState
   alias SmokexClient.Worker
   alias SmokexClient.Parsers.Yaml.Parser, as: YamlParser
 
   alias Smokex.PlanExecutions
   alias Smokex.PlanDefinition
+  alias Smokex.PlanExecution
 
-  @spec execute(PlanDefinition.t()) :: {:ok, term} | {:error, term}
-  def execute(%PlanDefinition{content: content} = plan_definition) do
+  @spec execute(PlanExecution.t()) :: {:ok, term} | {:error, term}
+  def execute(
+        %PlanExecution{
+          id: id,
+          status: :created,
+          plan_definition: %PlanDefinition{content: content}
+        } = plan_execution
+      ) do
     # TODO stop using a process to handle the state here
     ExecutionState.start_link()
 
-    {:ok, plan_execution} = PlanExecutions.create_plan_execution(plan_definition)
-
     # TODO this code is currently expecting a YAML file, but the parser could
     # be different depending on the type of content
-    plan_definition
-    |> Map.get(:content)
+    content
     |> YamlParser.parse()
     |> case do
       {:ok, list_of_requests} ->
@@ -28,10 +43,43 @@ defmodule SmokexClient.Executor do
           PlanExecutions.finish(plan_execution)
         catch
           {:error, reason} ->
+            Logger.error("Execution #{id} error: #{inspect(reason)}")
             PlanExecutions.halt(plan_execution)
         end
 
-      {:error, message} ->
+      {:error, reason} ->
+        Logger.error("Execution #{id} error: #{inspect(reason)}")
+        PlanExecutions.halt(plan_execution)
+    end
+  end
+
+  @spec execute(PlanDefinition.t()) :: {:ok, term} | {:error, term}
+  def execute(%PlanDefinition{content: content} = plan_definition) do
+    # TODO stop using a process to handle the state here
+    ExecutionState.start_link()
+
+    {:ok, plan_execution} = PlanExecutions.create_plan_execution(plan_definition)
+
+    # TODO this code is currently expecting a YAML file, but the parser could
+    # be different depending on the type of content
+    content
+    |> YamlParser.parse()
+    |> case do
+      {:ok, list_of_requests} ->
+        try do
+          PlanExecutions.start(plan_execution)
+
+          Enum.each(list_of_requests, &Worker.execute(&1, plan_execution))
+
+          PlanExecutions.finish(plan_execution)
+        catch
+          {:error, reason} ->
+            Logger.error("Execution #{plan_execution.id} error: #{inspect(reason)}")
+            PlanExecutions.halt(plan_execution)
+        end
+
+      {:error, reason} ->
+        Logger.error("Execution #{plan_execution.id} error: #{inspect(reason)}")
         PlanExecutions.halt(plan_execution)
     end
   end
--- apps/smokex_client/test/workers/default/worker_test.exs
+++ apps/smokex_client/test/workers/default/worker_test.exs
@@ -7,14 +7,17 @@ defmodule SmokexClient.Test.Workers.Default do
   alias Smokex.PlanExecution
 
   setup do
+    plan_definition = insert(:plan_definition, content: "- get: \n an: error")
+    plan_execution = insert(:plan_execution, plan_definition: plan_definition)
+
     [
-      plan_definition: insert(:plan_definition, content: "- get: \n an: error")
+      plan_execution: plan_execution
     ]
   end
 
   test "Given a yaml steps when launch worker then each valid step is processed", %{
-    plan_definition: plan_definition
+    plan_execution: plan_execution
   } do
-    assert {:ok, %PlanExecution{status: :halted}} = Executor.execute(plan_definition)
+    assert {:ok, %PlanExecution{status: :halted}} = Executor.execute(plan_execution)
   end
 end
