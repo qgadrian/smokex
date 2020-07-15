120a4fbdb8770f3ec96176d624aaf5c5f0680996
qgadrian <qgadrian@users.noreply.github.com>
2020-07-15 16:07:34 +0200

    Show progress details on execution view

--- apps/smokex_web/lib/smokex_web/live/plans_executions_live/show.ex
+++ apps/smokex_web/lib/smokex_web/live/plans_executions_live/show.ex
@@ -6,6 +6,7 @@ defmodule SmokexWeb.PlansExecutionsLive.Show do
   alias Smokex.PlanExecution
   alias Smokex.Result
   alias SmokexWeb.PlanExecutions.Components.StatusBadge
+  alias SmokexWeb.PlanExecutions.Components.Progress
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
