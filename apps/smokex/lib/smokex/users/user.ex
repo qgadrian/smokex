21ae9f23f07d74ed75b732a20a5baf4ba89d2335
qgadrian <qgadrian@users.noreply.github.com>
2020-07-18 17:38:11 +0200

    Add subscription expiration date to users

--- apps/smokex/lib/smokex/users/user.ex
+++ apps/smokex/lib/smokex/users/user.ex
@@ -2,9 +2,21 @@ defmodule Smokex.Users.User do
   use Ecto.Schema
   use Pow.Ecto.Schema
 
+  @optional_fields [:subscription_expires_at]
+
+  @schema_fields @optional_fields
+
   schema "users" do
+    field(:subscription_expires_at, :utc_datetime)
+
     pow_user_fields()
 
     timestamps()
+  end
+
+  def changeset(user_or_changeset, attrs) do
+    user_or_changeset
+    |> pow_changeset(attrs)
+    |> Ecto.Changeset.cast(attrs, @schema_fields)
   end
 end
