f8f42ce1c03012e8fabbc52fab6bc99081665a58
qgadrian <qgadrian@users.noreply.github.com>
2020-07-22 19:21:19 +0200

    Add function to create database

--- apps/smokex/lib/release.ex
+++ apps/smokex/lib/release.ex
@@ -3,7 +3,32 @@ defmodule Smokex.Release do
   Module that provides actions for release tasks.
   """
 
+  @start_apps [:postgrex, :ecto, :ecto_sql]
+
   @app :smokex
+
+  # https://elixirforum.com/t/how-to-create-database-on-release/28443/3
+  def create_database do
+    IO.puts("Starting dependencies...")
+
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
+
+    for repo <- repos do
+      :ok = ensure_repo_created(repo)
+    end
+
+    IO.puts("database created")
+  end
+
+  defp ensure_repo_created(repo) do
+    IO.puts("create #{inspect(repo)} database if it doesn't exist")
+
+    case repo.__adapter__.storage_up(repo.config) do
+      :ok -> :ok
+      {:error, :already_up} -> :ok
+      {:error, term} -> {:error, term}
+    end
+  end
 
   def migrate do
     for repo <- repos() do
