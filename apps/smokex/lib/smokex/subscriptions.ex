31545522b594c31ad17fcbbe81d6a71115f9e95c
qgadrian <qgadrian@users.noreply.github.com>
2020-07-28 13:36:51 +0200

    Create StripeSubscriptions table
    This will store the historic of subscriptions per user, allowing to get
    some metrics quickly and identify any potential problems with double
    subscriptions, for example.
    
    The intention of this table is just to keep track of the information,
    not to manage any subscription status.

--- apps/smokex/lib/smokex/subscriptions.ex
+++ apps/smokex/lib/smokex/subscriptions.ex
@@ -1,14 +1,47 @@
-defmodule Smokex.Subscriptions do
+defmodule Smokex.StripeSubscriptions do
+  alias Smokex.Users.User
+
+  alias Smokex.Subscriptions.StripeSubscription
   alias Smokex.Users.User
 
   @doc """
-  Whether the user can create a new plan definition.
+  Gets a subscription by the `customer id`.
 
-  In order to create a new plan definition the user has to have premium access
-  or meet the limited configuration.
+  If no subscription is found, raises an error.
   """
-  @spec renew_subscription(User.t()) :: boolean
-  def renew_subscription(%User{} = user) do
-    :tbi
+  @spec get_by(keyword) :: StripeSubscription.t()
+  def get_by(params) do
+    Smokex.Repo.get_by!(StripeSubscription, params)
+  end
+
+  @doc """
+  Creates a new user entry in the `stripe_subscriptions` table with the customer info.
+
+  This does not mean the user has an active subscription yet, but it creates
+  the information to track the user information is Stripe.
+  """
+  @spec create_customer(user :: User.t(), customer_id :: String.t()) ::
+          {:ok, User.t()} | {:error, Ecto.Changeset.t()}
+  def create_customer(%User{} = user, customer_id) when is_binary(customer_id) do
+    %StripeSubscription{}
+    |> StripeSubscription.create_changeset(%{
+      user: user,
+      customer_id: customer_id
+    })
+    |> Smokex.Repo.insert()
+  end
+
+  @doc """
+  Updates a Stripe subscription with the `subscription_id`.
+  """
+  @spec update_subscription(user :: User.t(), customer_id :: String.t()) ::
+          {:ok, StripeSubscription.t()} | {:error, Ecto.Changeset.t()}
+  def update_subscription(%StripeSubscription{} = user, subscription_id)
+      when is_binary(subscription_id) do
+    %StripeSubscription{}
+    |> StripeSubscription.update_changeset(%{
+      subscription_id: subscription_id
+    })
+    |> Smokex.Repo.insert()
   end
 end
--- apps/smokex/priv/repo/migrations/20200720164310_add_stripe_id_to_users.exs
+++
@@ -1,9 +0,0 @@
-defmodule Smokex.Repo.Migrations.AddStripeIdToUsers do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add(:stripe_customer_id, :string, null: true)
-    end
-  end
-end
