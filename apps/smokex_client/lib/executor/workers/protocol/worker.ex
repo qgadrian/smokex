a83ee9501856ff3a15153f51068eeb56fda84364
qgadrian <qgadrian@users.noreply.github.com>
2020-08-20 11:20:08 +0200

    Fix whole string being replaced by a variable

--- apps/smokex_client/lib/executor/workers/protocol/worker.ex
+++ apps/smokex_client/lib/executor/workers/protocol/worker.ex
@@ -9,6 +9,8 @@ defprotocol SmokexClient.Worker do
 end
 
 defimpl SmokexClient.Worker, for: Any do
+  alias SmokexClient.ExecutionContext
+
   @spec execute(any, Smokex.PlanExecution.t(), ExecutionContext.t()) :: atom
   def execute(_step, _plan_execution, _execution_context) do
     # TODO do not throw are return error flow tuples
