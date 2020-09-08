2d801b1b04c26e695fe5e9c029d78fa928e45b71
qgadrian <qgadrian@users.noreply.github.com>
2020-09-08 17:07:55 +0200

    Add helper function to reload a user in sessions
    When a user is modified this function will update all active sessions in
    the CredentialsCacheStore

--- apps/smokex_web/lib/smokex_web/session_helper.ex
+++ apps/smokex_web/lib/smokex_web/session_helper.ex
@@ -40,8 +40,71 @@ defmodule SmokexWeb.SessionHelper do
   def sync_user(conn, user), do: Pow.Plug.create(conn, user)
 
   @doc """
-  Retrieves the currently-logged-in user from the Pow credentials cache.
+  Reloads all active session with the update user information from the
+  database.
+
+  This function receives a `t:Phoenix.LiveView.Socket.t/0` and a active session
+  and returns the updated socket with the new `current_user` information.
+
+  For more info see:
+  * https://hexdocs.pm/pow/sync_user.html#reload-the-user
+  * https://hexdocs.pm/pow/sync_user.html#update-user-in-the-credentials-cache
+  * https://github.com/danschultzer/pow/issues/271#issuecomment-621979869
   """
+  @spec reload_user(Phoenix.LiveView.Socket.t(), keyword()) :: :ok
+  def reload_user(socket, %{"smokex_web_auth" => signed_token} = session, opts \\ []) do
+    Logger.info("Reload user in credentials cache")
+
+    conn = struct!(Plug.Conn, secret_key_base: socket.endpoint.config(:secret_key_base))
+
+    pow_config = [otp_app: :smokex_web]
+    salt = Atom.to_string(Pow.Plug.Session)
+
+    with {:ok, token} <- Pow.Plug.verify_token(conn, salt, signed_token, pow_config),
+         {user, metadata} <- CredentialsCache.get([backend: Pow.Store.Backend.EtsCache], token),
+         {user, _metadata} = pow_credential <- {Smokex.Repo.get!(User, user.id), metadata},
+         :ok <- update_sessions(pow_config, token, pow_credential),
+         user <- maybe_preload_fields(user, opts[:preload]) do
+      Phoenix.LiveView.assign(socket, current_user: user)
+    else
+      any ->
+        socket
+    end
+  end
+
+  #
+  # Private functions
+  #
+
+  @spec maybe_preload_fields(User.t(), keyword | atom | nil) :: User.t()
+  defp maybe_preload_fields(%User{} = user, nil), do: user
+  defp maybe_preload_fields(%User{} = user, []), do: user
+
+  defp maybe_preload_fields(%User{} = user, preload) do
+    Smokex.Repo.preload(user, preload)
+  end
+
+  @spec update_sessions(
+          pow_config :: keyword,
+          session_token :: String.t(),
+          {User.t(), metadata :: term}
+        ) :: :ok
+  defp update_sessions(pow_config, session_token, {%User{} = user, metadata} = pow_credential) do
+    sessions = CredentialsCache.sessions(pow_config, user)
+
+    # Do we have an available session which matches the fingerprint?
+    sessions
+    |> Enum.find(&(&1 == session_token))
+    |> case do
+      nil ->
+        Logger.debug("No Matching Session Found")
+
+      available_session ->
+        Logger.debug("Matching session found, updating credential")
+        CredentialsCache.put(pow_config, session_token, pow_credential)
+    end
+  end
+
   @spec get_user(
           socket :: Socket.t(),
           session :: map(),
@@ -68,17 +131,5 @@ defmodule SmokexWeb.SessionHelper do
     Logger.warn("Cannot retrieve user from session, request not matched.")
 
     nil
-  end
-
-  #
-  # Private functions
-  #
-
-  @spec maybe_preload_fields(User.t(), keyword | atom | nil) :: User.t()
-  defp maybe_preload_fields(%User{} = user, nil), do: user
-  defp maybe_preload_fields(%User{} = user, []), do: user
-
-  defp maybe_preload_fields(%User{} = user, preload) do
-    Smokex.Repo.preload(user, preload)
   end
 end
