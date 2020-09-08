17f579f48e8165ac705e666f71ee4f477e0aca61
qgadrian <qgadrian@users.noreply.github.com>
2020-09-08 18:38:58 +0200

    Add module to  send notifications

--- apps/smokex/lib/smokex/notifications.ex
+++ apps/smokex/lib/smokex/notifications.ex
@@ -1,6 +1,10 @@
 defmodule Smokex.Notifications do
   @moduledoc """
   Handles the notifications in the system.
+
+  So far the following notifications are enabled:
+
+  * Slack
   """
 
   require Logger
@@ -11,35 +15,27 @@ defmodule Smokex.Notifications do
   alias Smokex.PlanExecution
   alias Smokex.Users.User
 
-  # def maybe_notify_execution({:ok, %PlanExecution{} = plan_execution} = result) do
-  # maybe_notify_slack(plan_execution, :ok)
+  @doc """
+  Notifies a change in a execution.
 
-  # result
-  # end
-
-  # def maybe_notify_execution({:error, %PlanExecution{} = plan_execution} = result) do
-  # maybe_notify_slack(plan_execution, :error)
-
-  # result
-  # end
-
-  # def maybe_notify_execution(result) do
-  # Logger.warn("Unknown result: #{inspect(result)}")
-  # end
-
+  This function receives and returns a tuple with a result on a
+  `t:#{PlanExecution}.t/0` change.
+  """
+  @spec maybe_notify_change({:ok, PlanExecution.t()} | term) :: {:ok, PlanExecution.t()} | term
   def maybe_notify_change({:ok, %PlanExecution{} = plan_execution} = result) do
     maybe_notify_slack(plan_execution)
 
     result
   end
 
-  def maybe_notify_change(error), do: error |> IO.inspect()
+  def maybe_notify_change(error), do: error
 
   #
   # Private functions
   #
 
-  def maybe_notify_slack(%PlanExecution{} = plan_execution) do
+  @spec maybe_notify_slack(PlanExecution.t()) :: :ok
+  defp maybe_notify_slack(%PlanExecution{} = plan_execution) do
     %PlanExecution{status: status, plan_definition: %PlanDefinition{users: [user]}} =
       Smokex.Repo.preload(plan_execution, plan_definition: :users)
 
@@ -48,6 +44,7 @@ defmodule Smokex.Notifications do
     do_maybe_notify_slack(slack_integration, status)
   end
 
+  @spec do_maybe_notify_slack(SlackUserIntegration.t(), PlanExecution.status()) :: :ok
   defp do_maybe_notify_slack(nil, _), do: :ok
 
   defp do_maybe_notify_slack(%SlackUserIntegration{} = slack_integration, :running) do
