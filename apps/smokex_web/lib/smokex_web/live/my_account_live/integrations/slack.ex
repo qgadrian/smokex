8d5f576f98675605e76e7a901075c99b62883650
qgadrian <qgadrian@users.noreply.github.com>
2020-09-08 17:15:09 +0200

    Add option to remove Slack integration

--- apps/smokex_web/lib/smokex_web/live/my_account_live/integrations/slack.ex
+++ apps/smokex_web/lib/smokex_web/live/my_account_live/integrations/slack.ex
@@ -13,6 +13,7 @@ defmodule SmokexWeb.MyAccountLive.Integrations.Slack do
     socket =
       socket
       |> SessionHelper.assign_user!(session, preload: :slack_integration)
+      |> assign(:session, session)
       |> maybe_fetch_options()
 
     {:ok, socket}
@@ -26,15 +27,40 @@ defmodule SmokexWeb.MyAccountLive.Integrations.Slack do
   @impl Phoenix.LiveView
   def handle_event(
         "save",
-        %{"preferences" => preferences_attrs},
+        %{"slack_integration_preferences" => preferences_attrs},
         %Socket{assigns: %{current_user: current_user}} = socket
       ) do
     case Slack.update_preferences(current_user, preferences_attrs) do
-      {:ok, preferences} ->
-        {:noreply, assign(socket, changeset: preferences)}
+      {:ok, %SlackUserIntegration{options: preferences}} ->
+        changeset = Ecto.Changeset.change(preferences)
+        {:noreply, assign(socket, changeset: changeset)}
 
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event(
+        "remove_integration",
+        _params,
+        %Socket{assigns: %{current_user: current_user, session: session}} = socket
+      ) do
+    case Slack.remove_integration(current_user) do
+      :ok ->
+        socket = SessionHelper.reload_user(socket, session, preload: :slack_integration)
+
+        {:noreply, socket}
+
+      :error ->
+        socket =
+          put_flash(
+            socket,
+            :error,
+            "Error removing the Slack integration, please contact support@smokex.io"
+          )
+
+        {:noreply, socket}
     end
   end
 
@@ -54,7 +80,12 @@ defmodule SmokexWeb.MyAccountLive.Integrations.Slack do
          } = socket
        ) do
     changeset = Ecto.Changeset.change(preferences)
-
     assign(socket, changeset: changeset)
+  end
+
+  defp maybe_fetch_options(
+         %Socket{assigns: %{current_user: %User{slack_integration: nil}}} = socket
+       ) do
+    socket
   end
 end
