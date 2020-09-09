38aa3b3e072f8aa982b714a760f78c6097940a2f
qgadrian <qgadrian@users.noreply.github.com>
2020-09-09 20:01:53 +0200

    Add env var to explicitly enable the datadog agent

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-datadog-agent run & \
-/opt/datadog-agent/embedded/bin/trace-agent & \
-/opt/datadog-agent/embedded/bin/process-agent &
+if [ $ENABLE_DATADOG_AGENT == 'true' ]; then
+  datadog-agent run & \
+  /opt/datadog-agent/embedded/bin/trace-agent & \
+  /opt/datadog-agent/embedded/bin/process-agent &
+fi
 
 exec "$@"
