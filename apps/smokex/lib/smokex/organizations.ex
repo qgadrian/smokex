e6641752ce6f84b011baa89c017811dfc6e5180f
qgadrian <qgadrian@users.noreply.github.com>
2020-09-10 20:10:37 +0200

    Move the `is_subscribed?` logic to Organizations
    The `Organizations` context is the one handling the logic to check the
    subscriptions since the subscription information is now linked to an
    organization and not by user.

--- apps/smokex/lib/smokex/organizations.ex
+++ apps/smokex/lib/smokex/organizations.ex
@@ -16,4 +16,35 @@ defmodule Smokex.Organizations do
     |> Organization.create_changeset(%{users: [user]})
     |> Smokex.Repo.insert()
   end
+
+  @doc """
+  Returns whether the organization has an premium access to the application.
+
+  If the organization is not subscribed, then the `free_access` config is
+  checked as fallback to grant access to premium features.
+  """
+  @spec subscribed?(Organization.t()) :: boolean
+  def subscribed?(nil) do
+    Application.get_env(:smokex, :free_access, false)
+  end
+
+  def subscribed?(%Organization{subscription_expires_at: nil}) do
+    Application.get_env(:smokex, :free_access, false)
+  end
+
+  def subscribed?(%Organization{subscription_expires_at: subscription_expires_at}) do
+    is_free_access = Application.get_env(:smokex, :free_access, false)
+
+    if is_free_access do
+      true
+    else
+      subscription_expires_at
+      |> DateTime.compare(DateTime.utc_now())
+      |> case do
+        :gt -> true
+        :eq -> true
+        :lt -> false
+      end
+    end
+  end
 end
