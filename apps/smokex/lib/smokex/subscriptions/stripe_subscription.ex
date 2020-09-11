db35ecf312dfc43a1064f9a27ab3b73b304ccee2
qgadrian <qgadrian@users.noreply.github.com>
2020-09-11 16:45:07 +0200

    Attach organizations to Stripe subscriptions

--- apps/smokex/lib/smokex/subscriptions/stripe_subscription.ex
+++ apps/smokex/lib/smokex/subscriptions/stripe_subscription.ex
@@ -4,23 +4,23 @@ defmodule Smokex.Subscriptions.StripeSubscription do
   """
   use Ecto.Schema
 
-  alias Smokex.Users.User
+  alias Smokex.Organizations.Organization
 
   @typedoc """
   Represents a [stripe subscription](https://stripe.com/docs/api/subscriptions):
 
   * `customer_id`: The [stripe customer](https://stripe.com/docs/api/customers/object).
   * `stripe_id`: The [stripe subscription](https://stripe.com/docs/api/subscriptions/object).
-  * `user`: The user who the subscription belongs to.
+  * `organization`: The organization who the subscription belongs to.
   """
   @type t :: %__MODULE__{
           customer_id: String.t(),
           subscription_id: String.t(),
-          user: User.t()
+          organization: Organization.t()
         }
 
   @required_fields [:customer_id]
-  @optional_fields [:subscription_id, :user_id]
+  @optional_fields [:subscription_id, :organization_id]
   @updatable_fields [:subscription_id]
 
   @schema_fields @optional_fields ++ @required_fields
@@ -29,7 +29,7 @@ defmodule Smokex.Subscriptions.StripeSubscription do
     field(:customer_id, :string, null: false)
     field(:subscription_id, :string, null: true)
 
-    belongs_to(:user, User)
+    belongs_to(:organization, Organization)
 
     timestamps()
   end
@@ -39,7 +39,7 @@ defmodule Smokex.Subscriptions.StripeSubscription do
     changeset
     |> Ecto.Changeset.cast(params, @schema_fields)
     |> Ecto.Changeset.validate_required(@required_fields)
-    |> maybe_put_user(params)
+    |> maybe_put_organization(params)
   end
 
   @spec update_changeset(__MODULE__.t(), map) :: Ecto.Changeset.t()
@@ -52,13 +52,13 @@ defmodule Smokex.Subscriptions.StripeSubscription do
   # Private functions
   #
 
-  defp maybe_put_user(changeset, %{user: nil}), do: changeset
+  defp maybe_put_organization(changeset, %{organization: nil}), do: changeset
 
-  defp maybe_put_user(changeset, %{user: %User{} = user}) do
+  defp maybe_put_organization(changeset, %{organization: %Organization{} = organization}) do
     changeset
-    |> Ecto.Changeset.put_assoc(:user, user)
-    |> Ecto.Changeset.assoc_constraint(:user)
+    |> Ecto.Changeset.put_assoc(:organization, organization)
+    |> Ecto.Changeset.assoc_constraint(:organization)
   end
 
-  defp maybe_put_user(changeset, _params), do: changeset
+  defp maybe_put_organization(changeset, _params), do: changeset
 end
--- apps/smokex/priv/repo/migrations/20200725122704_create_stripe_subscriptions.exs
+++ apps/smokex/priv/repo/migrations/20200725122704_create_stripe_subscriptions.exs
@@ -3,7 +3,7 @@ defmodule Smokex.Repo.Migrations.CreateStripeSubscriptions do
 
   def change do
     create table(:stripe_subscriptions) do
-      add(:user_id, references(:users), null: true)
+      add(:organization_id, references(:organizations), null: true)
 
       add(:customer_id, :string, null: false)
       add(:subscription_id, :string, null: true)
@@ -11,6 +11,6 @@ defmodule Smokex.Repo.Migrations.CreateStripeSubscriptions do
       timestamps()
     end
 
-    create unique_index(:stripe_subscriptions, [:user_id, :customer_id, :subscription_id])
+    create unique_index(:stripe_subscriptions, [:organization_id, :customer_id, :subscription_id])
   end
 end
