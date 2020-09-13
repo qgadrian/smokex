b191ed681373c960aa9739b7ac5f5706c27cff84
qgadrian <qgadrian@users.noreply.github.com>
2020-09-13 21:59:39 +0200

    Add require tag id

--- apps/smokex_web/test/smokex_web/live/plans_executions_live/list_test.exs
+++ apps/smokex_web/test/smokex_web/live/plans_executions_live/list_test.exs
@@ -1,7 +1,6 @@
 defmodule SmokexWeb.PlansExecutionsLive.ListTest do
   use SmokexWeb.ConnCase, async: true
 
-  # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.LiveViewTest
 
   import Smokex.TestSupport.Factories
