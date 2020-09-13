c94a4dc5a1a4abb429675c2ccc5d48954bf8c995
qgadrian <qgadrian@users.noreply.github.com>
2020-09-13 12:16:31 +0200

    Fix missing spec and matchings

--- apps/smokex_web/lib/smokex_web/telemetry/oban_error_reporter.ex
+++ apps/smokex_web/lib/smokex_web/telemetry/oban_error_reporter.ex
@@ -1,19 +1,34 @@
 defmodule SmokexWeb.Telemetry.ObanErrorReporter do
+  @moduledoc """
+  This module handles Oban telemetry error event thrown by workers.
+  """
+
+  require Logger
+
+  @spec handle_event(
+          :telemetry.event_name(),
+          :telemetry.event_measurements(),
+          :telemetry.event_metadata(),
+          term
+        ) :: :ok
   def handle_event([:oban, :job, :exception], measure, meta, _) do
     extra =
       meta
       |> Map.take([:id, :args, :queue, :worker])
       |> Map.merge(measure)
 
-    log_and_report(meta)
+    log_and_report(meta, extra)
   end
 
   def handle_event([:oban, :circuit, :trip], _measure, meta, _) do
     log_and_report(meta)
   end
 
-  defp log_and_report(meta) do
-    Logger.error("#{inspect(meta)}")
+  @spec log_and_report(:telemetry.event_metadata(), map) :: :ok
+  defp log_and_report(meta, extra \\ %{}) do
+    Logger.error("#{inspect(meta)}", extra: extra)
     Sentry.capture_exception(meta.error, stracktrace: meta.stacktrace, extra: extra)
+
+    :ok
   end
 end
