b3ff6bb992009977f8e63ae8c3ab8191eb3cce09
qgadrian <qgadrian@users.noreply.github.com>
2020-09-14 20:10:57 +0200

    Show pricing pages using the limits configurations

--- apps/smokex_web/lib/smokex_web/live/pricing_live/show.ex
+++ apps/smokex_web/lib/smokex_web/live/pricing_live/show.ex
@@ -14,4 +14,42 @@ defmodule SmokexWeb.PricingLive.Show do
   def handle_params(_params, _url, socket) do
     {:noreply, socket}
   end
+
+  #
+  # Private functions
+  #
+
+  defp max_plan_definitions,
+    do: Application.get_env(:smokex, :limit_plan_definitions_per_organization)
+
+  defp max_limit_executions_per_period,
+    do: Application.get_env(:smokex, :limit_executions_per_period)
+
+  defp max_limit_executions_expires_after_hours,
+    do: Application.get_env(:smokex, :limit_executions_expires_after_hours)
+
+  # TODO remove this crap after using gettext with plurals
+  defp plural(:hour) do
+    if Application.get_env(:smokex, :limit_executions_expires_after_hours) > 1 do
+      "hours"
+    else
+      "hour"
+    end
+  end
+
+  defp plural(:execution) do
+    if Application.get_env(:smokex, :limit_executions_per_period) > 1 do
+      "executions"
+    else
+      "execution"
+    end
+  end
+
+  defp plural(:plan) do
+    if Application.get_env(:smokex, :limit_plan_definitions_per_organization) > 1 do
+      "plans"
+    else
+      "plan"
+    end
+  end
 end
