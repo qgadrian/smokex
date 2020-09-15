d7b33bbc2f1c643d069e3827094a55801c65e54b
qgadrian <qgadrian@users.noreply.github.com>
2020-09-15 18:49:14 +0200

    Add test for executions table live view

--- apps/smokex_web/test/smokex_web/live/plans_executions_live/list_test.exs
+++ apps/smokex_web/test/smokex_web/live/plans_executions_live/list_test.exs
@@ -10,7 +10,7 @@ defmodule SmokexWeb.PlansExecutionsLive.ListTest do
     setup %{conn: conn} do
       organization = insert(:organization)
       user = insert(:user, organizations: [organization])
-      _plan_definition = insert(:plan_definition, author: user, organization: organization)
+      plan_definition = insert(:plan_definition, author: user, organization: organization)
 
       conn =
         conn
@@ -18,10 +18,22 @@ defmodule SmokexWeb.PlansExecutionsLive.ListTest do
         |> Pow.Plug.Session.call(otp_app: :smokex_web)
         |> SmokexWeb.SessionHelper.sync_user(user)
 
-      {:ok, conn: conn}
+      {:ok, conn: conn, plan_definition: plan_definition}
     end
 
-    test "renders the executions", %{conn: conn} do
+    test "sets the correct id to the executions list element", %{
+      conn: conn,
+      plan_definition: plan_definition
+    } do
+      status = "created"
+
+      {:ok, _view, html} = live(conn, "/executions?status=#{status}&plan=#{plan_definition.id}")
+
+      assert html =~ "<ul id=\"#{plan_definition.id}-#{status}\" phx-update=\"append\">"
+      refute html =~ "<h2 class=\"title\">Get started with Smokex Plans</h2>"
+    end
+
+    test "renders all the executions", %{conn: conn} do
       {:ok, _view, html} = live(conn, "/executions")
 
       assert html =~ "<ul id=\"-all\" phx-update=\"append\">"
