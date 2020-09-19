b1d3d2606728fa0cd860902dd411e748bb199ee6
qgadrian <qgadrian@users.noreply.github.com>
2020-09-19 17:59:07 +0200

    Add button to delete secrets

--- apps/smokex_web/lib/smokex_web/live/my_account_live/administration/secrets/show.ex
+++ apps/smokex_web/lib/smokex_web/live/my_account_live/administration/secrets/show.ex
@@ -4,6 +4,7 @@ defmodule SmokexWeb.MyAccountLive.Administration.Secrets.Show do
   alias Phoenix.LiveView.Socket
   alias SmokexWeb.MyAccountLive.Components.SideMenu
   alias Smokex.OrganizationsSecrets
+  alias Smokex.Organizations.Secret
   alias Smokex.Organizations
   alias Smokex.Organizations.Organization
 
@@ -12,7 +13,7 @@ defmodule SmokexWeb.MyAccountLive.Administration.Secrets.Show do
     socket =
       socket
       |> SessionHelper.assign_user!(session)
-      |> assign_secrets()
+      |> fetch_secrets()
 
     {:ok, socket}
   end
@@ -22,11 +23,32 @@ defmodule SmokexWeb.MyAccountLive.Administration.Secrets.Show do
     {:noreply, socket}
   end
 
+  @impl Phoenix.LiveView
+  def handle_event(
+        "delete",
+        %{"id" => secret_id},
+        %Socket{assigns: %{current_user: user}} = socket
+      ) do
+    with secret_id <- String.to_integer(secret_id),
+         {:ok, %Organization{} = organization} <- Organizations.get_organization(user),
+         %Secret{} = secret <- OrganizationsSecrets.get!(organization, secret_id),
+         {:ok, _secret} <- OrganizationsSecrets.delete(secret) do
+      socket = fetch_secrets(socket)
+
+      {:noreply, socket}
+    else
+      {:error, %Ecto.Changeset{} = changeset} ->
+        socket = put_flash(socket, :error, "Error deleting the secret, please contact support")
+
+        {:noreply, socket}
+    end
+  end
+
   #
   # Private functions
   #
 
-  defp assign_secrets(%Socket{assigns: %{current_user: user}} = socket) do
+  defp fetch_secrets(%Socket{assigns: %{current_user: user}} = socket) do
     {:ok, %Organization{} = organization} = Organizations.get_organization(user)
 
     secrets = OrganizationsSecrets.list(organization)
