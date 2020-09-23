40c9e18514907641c3337a342e9c59f499f67e35
Alvaro Gomez Traveso <agtraveso@gmail.com>
2020-09-23 18:25:14 +0200

    restyle execution details

--- apps/smokex_web/lib/smokex_web/views/plans_executions/components/status_badge.ex
+++ apps/smokex_web/lib/smokex_web/views/plans_executions/components/status_badge.ex
@@ -3,21 +3,30 @@ defmodule SmokexWeb.PlanExecutions.Components.StatusBadge do
 
   alias Smokex.PlanExecution
 
-  @default_class "tag is-small"
+  @default_class "is-circle"
+  @default_icon_class "has-text-white"
 
   def new(%PlanExecution{status: :created}) do
-    content_tag(:span, "Created", class: "#{@default_class} is-dark")
+    content_tag(:span, class: "has-background-dark #{@default_class}") do
+      content_tag('ion-icon', "", name: "remove-outline", class: @default_icon_class)
+    end
   end
 
   def new(%PlanExecution{status: :finished}) do
-    content_tag(:span, "Finished", class: "#{@default_class} is-success")
+    content_tag(:span, class: "has-background-success #{@default_class}") do
+      content_tag('ion-icon', "", name: "checkmark-outline", class: @default_icon_class)
+    end
   end
 
   def new(%PlanExecution{status: :halted}) do
-    content_tag(:span, "Halted", class: "#{@default_class} is-danger")
+    content_tag(:span, class: "has-background-danger #{@default_class}") do
+      content_tag('ion-icon', "", name: "close-outline", class: @default_icon_class)
+    end
   end
 
   def new(%PlanExecution{status: :running}) do
-    content_tag(:span, "Running", class: "#{@default_class} is-info")
+    content_tag(:span, class: "has-background-info #{@default_class}") do
+      content_tag('ion-icon', "", name: "sync-outline", class: "icn-spinner #{@default_icon_class}")
+    end
   end
 end
