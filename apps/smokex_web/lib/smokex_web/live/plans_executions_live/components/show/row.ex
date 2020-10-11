6efb8e5c31486c43c2247a2d6b92f8ff47e4d8ad
qgadrian <qgadrian@users.noreply.github.com>
2020-10-11 14:40:45 +0200

    Simplify expectation parsing into String

--- apps/smokex_web/lib/smokex_web/live/plans_executions_live/components/show/row.ex
+++ apps/smokex_web/lib/smokex_web/live/plans_executions_live/components/show/row.ex
@@ -17,68 +17,15 @@ defmodule SmokexWeb.PlansExecutionsLive.Components.Show.Row do
   #
 
   @doc """
-  Converts a expected value from an assertion.
-
-  The expected value can be:
-
-  * A map or a list or received elements
-  * A list of received headers
+  Converts a expected/received value from an assertion.
   """
-  @spec expect_value_to_string(failed_assertions :: map) :: String.t()
-  def expect_value_to_string(%{"expected" => expected})
+  @spec value_to_string(expected_value :: term) :: String.t() | number | boolean
+  def value_to_string(expected)
       when is_map(expected) or is_list(expected) do
     Jason.encode!(expected, pretty: true)
   end
 
-  def expect_value_to_string(expected_list) when is_list(expected_list) do
-    Enum.map(expected_list, fn
-      %{"expected" => expected, "header" => header_name} ->
-        "#{header_name}: #{expected}\n"
+  def value_to_string(nil), do: "-"
 
-      %{expected: expected, header: header_name} ->
-        "#{header_name}: #{expected}\n"
-    end)
-  end
-
-  def expect_value_to_string(%{"expected" => expected}) do
-    inspect(expected)
-  end
-
-  def expect_value_to_string(message), do: inspect(message)
-
-  @doc """
-  Converts a received value from an assertion.
-
-  The received value can be:
-
-  * A map or a list or received elements
-  * A list of received headers
-  """
-  @spec received_value_to_string(failed_assertions :: map) :: String.t()
-  def received_value_to_string(%{"received" => received})
-      when is_map(received) or is_list(received) do
-    Jason.encode!(received, pretty: true)
-  end
-
-  def received_value_to_string(received_list) when is_list(received_list) do
-    Enum.map(received_list, fn
-      %{"received" => nil, "header" => header_name} ->
-        "`#{header_name}` not present\n"
-
-      %{"received" => received, "header" => header_name} ->
-        "#{header_name}: #{received}\n"
-
-      %{received: nil, header: header_name} ->
-        "`#{header_name}` not present\n"
-
-      %{received: received, header: header_name} ->
-        "#{header_name}: #{received}\n"
-    end)
-  end
-
-  def received_value_to_string(%{"received" => received}) do
-    inspect(received)
-  end
-
-  def received_value_to_string(message), do: inspect(message)
+  def value_to_string(expected), do: expected
 end
