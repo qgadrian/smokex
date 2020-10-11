d9070058b8960de4cbebc43303bbdc68e0d612ef
qgadrian <qgadrian@users.noreply.github.com>
2020-10-11 14:03:43 +0200

    Avoid send datadog to /dev/null
    There is a datadog configuration to set the agent log level at runtime

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 if [ $ENABLE_DATADOG_AGENT == 'true' ]; then
   datadog-agent run & \
   /opt/datadog-agent/embedded/bin/trace-agent & \
-  /opt/datadog-agent/embedded/bin/process-agent > /dev/null 2>&1 &
+  /opt/datadog-agent/embedded/bin/process-agent &
 fi
 
 exec "$@"
