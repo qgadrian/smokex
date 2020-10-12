808e714953f086ddc756f1739bccda81e3049551
qgadrian <qgadrian@users.noreply.github.com>
2020-10-12 13:26:39 +0200

    Add alias to run seed file

--- apps/smokex/priv/repo/migrations/20200701175658_create_plans_executions_http_request_results.exs
+++ apps/smokex/priv/repo/migrations/20200701175658_create_plans_executions_http_request_results.exs
@@ -3,7 +3,9 @@ defmodule Smokex.Repo.Migrations.CreatePlansExecutionsStepsResults do
 
   def change do
     create table(:plans_executions_http_request_results) do
-      add(:plan_execution_id, references(:plans_executions, type: :uuid, on_delete: :delete_all), null: false)
+      add(:plan_execution_id, references(:plans_executions, type: :uuid, on_delete: :delete_all),
+        null: false
+      )
 
       add(:action, :string, null: true)
       add(:host, :string, null: true)
